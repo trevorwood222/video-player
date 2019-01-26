a026035e42bf555f59745f82fb3c81701543fdee
trevoros <trevor@originalstitch.com>
2019-01-26 11:47:07 +0900

    Init

---
+++ video-player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  video-player
+//
+//  Created by Trevor Wood on 2019/01/26.
+//  Copyright © 2019 Trevor Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
